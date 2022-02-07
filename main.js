8b1c44686261ab192f7483ef589a1db8026a0b23
Paul Reece <paulreece2@gmail.com>
2022-02-06 20:25:26 -0500

    with timer

--- main.js
+++ main.js
@@ -56,4 +56,34 @@ function resetBoard() {
     })
 }) ();
 
+
+
 items.forEach(item => item.addEventListener('click', flipCard));
+
+
+
+let minutesLabel = document.getElementById('minute')
+
+let secondsLabel = document.getElementById('second')
+
+let totalSeconds = 0
+
+
+setInterval(setTime, 1000)
+
+function setTime() {
+    ++totalSeconds
+    secondsLabel.innerHTML = pad(totalSeconds%60)
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds/60))
+}
+
+function pad(val) {
+    let valString = val + ''
+    if(valString.length < 2) {
+        return '0' + valString;
+    } else {
+        return valString
+    }
+}
+
+
